85c6e35e322ac86909d81eaa30c74e41b9ad73fb
Rohan2596 <rohankadam662>
2019-04-05 19:28:19 +0530

    java login Application

--- LoginRegistration2/src/com/Bridgelabz/SERVICE/CustomerDAO.java
+++
@@ -1,6 +0,0 @@
-package com.Bridgelabz.SERVICE;
-import com.Bridgelabz.MODEL.*;
-public interface CustomerDAO {
-	public int insertCustomer(Customer c) ;
-	public Customer getCustomer(String userId,String password);
-}
--- LoginRegistration2/src/com/Bridgelabz/SERVICE/MyProvider.java
+++
@@ -1,7 +0,0 @@
-package com.Bridgelabz.SERVICE;
-
-public interface MyProvider {
-	String Uname = "root";
-	String Pword = "test";
-	String Url = "jdbc:mysql://localhost:3306/CUSTOMER";
-}
